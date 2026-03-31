a6c1e7027ad5a45ae5034d43e24fdf984763b681
BiographyLibrary <claudio@brignole.ch>
2026-03-31 23:42:49 +0200

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,54 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV !== 'production',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'google-fonts-stylesheets',
-        expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'google-fonts-webfonts',
-        expiration: { maxEntries: 30, maxAgeSeconds: 60 * 60 * 24 * 365 },
-      },
-    },
-    {
-      urlPattern: /\/api\/.*/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'api-routes',
-        expiration: { maxEntries: 32, maxAgeSeconds: 60 * 60 * 24 },
-        networkTimeoutSeconds: 10,
-      },
-    },
-    {
-      urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|ico)$/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'static-images',
-        expiration: { maxEntries: 64, maxAgeSeconds: 60 * 60 * 24 * 30 },
-      },
-    },
-    {
-      urlPattern: /\.(?:js|css)$/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'static-assets',
-        expiration: { maxEntries: 64, maxAgeSeconds: 60 * 60 * 24 * 30 },
-      },
-    },
-  ],
-});
-
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -56,4 +6,4 @@ const nextConfig = {
   images: { unoptimized: true },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
